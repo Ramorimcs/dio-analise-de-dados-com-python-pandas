f9e71f06e8124d13b26e4c44bb984895b14bdbd1
Ramorimcs <ramorimcs@gmail.com>
2022-10-16 22:33:28 -0300

    atualizado .py

--- analise-exploratoria.py
+++ analise-exploratoria.py
@@ -1,4 +1,6 @@
 #importando as bibliotecas
+from turtle import title
+from xml.dom import registerDOMImplementation
 import pandas as pd
 import matplotlib.pyplot as plt
 plt.style.use("seaborn")
@@ -23,4 +25,38 @@ df["Prazo Entrega"].dtype
 df["Prazo Entrega"].groupby(df["Marca"]).mean()
 #Pergunta 6 - Lucro por ano E por marca?
 df["Ano"] = df["Data Venda"].dt.year
+pd.options.display.float_format = '{:20,.2f}'.format
 df["Lucro Total"].groupby([df["Ano"], df["Marca"]]).sum()
+df["Lucro Total"].groupby([df["Ano"], df["Marca"]]).sum().reset_index()
+#Pergunta 7 - Total de produtos vendidos?
+df["Quantidade"].sum()
+df["Quantidade"].groupby(df["Produto"]).sum().sort_values(ascending=False).reset_index()
+#Pergunta 8 - Total de Vendas apenas de 2009?
+df[df["Ano"]==2009]["Valor Venda"].head()
+df[df["Ano"]==2009]["Valor Venda"].groupby(df["Data Venda"].dt.month).sum()
+
+#Pergunta 9 - Plots
+#9.1 Gráfico Lucro Mensal em 2009
+df[df["Ano"]==2009]["Valor Venda"].groupby(df["Data Venda"].dt.month).sum().plot(title="Lucro Mensal em 2009")
+plt.xlabel("Mês")
+plt.ylabel("Lucro")
+#9.2 Gráfico Lucro por Marca em 2009
+df[df["Ano"]==2009]["Lucro Total"].groupby(df["Marca"]).sum().plot.bar(title="Lucro por Marca em 2009")
+plt.xlabel("Marca")
+plt.ylabel("Lucro")
+plt.xticks(rotation='horizontal')
+#9.3 Gráfico Lucro por Classe em 2009
+df[df["Ano"]==2009]["Lucro Total"].groupby(df["Classe"]).sum().plot.bar(title="Lucro por Classe em 2009")
+plt.xlabel("Classe")
+plt.ylabel("Lucro")
+plt.xticks(rotation='horizontal')
+#9.4 Gráfico Total de Produtos Vendidos por ano
+df["Quantidade"].groupby(df["Produto"]).sum().sort_values(ascending=False).plot.barh(title="Total Produtos Vendidos")
+plt.xlabel("Total")
+plt.ylabel("Produto")
+#9.5 Gráfico Lucro por ano
+df["Lucro Total"].groupby(df["Ano"]).sum().sort_values(ascending=False).plot.bar(title="Lucro x Ano")
+plt.xlabel("Lucro")
+plt.ylabel("Ano")
+
+
